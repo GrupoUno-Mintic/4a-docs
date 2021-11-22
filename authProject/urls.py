10cf5e9e734cfcd4098d2b477025bfe5665e275e
Plozada <palozadagutierrez@gmail.com>
2021-11-21 22:10:08 -0500

    Incluir metodo Get para todos los usuarios y metodo Put y Delete para 1 usuario

--- authProject/urls.py
+++ authProject/urls.py
@@ -17,10 +17,13 @@ from django.urls import path
 from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
 from authApp import views
 
+
+
 urlpatterns = [
     path('login/', TokenObtainPairView.as_view()),
     path('refresh/', TokenRefreshView.as_view()),
     path('verifyToken/', views.VerifyTokenView.as_view()),
     path('user/', views.UserCreateView.as_view()),
-    path('user/<int:pk>/', views.UserDetailView.as_view()),
+    path('userView/<int:pk>/', views.UserView.as_view()), 
+    
 ]
